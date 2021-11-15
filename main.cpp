d8274167364e40cfcd462c1b9612bdf4370070aa
Jochen Mehlich <contact@jochenmehlich.de>
2021-11-15 13:08:00 +0100

    übung 8

--- main.cpp
+++ main.cpp
@@ -2,5 +2,45 @@
 
 int main(void){
 
+    int felda[4];
+    int feldb[5];
+    int feldc[9];
+    for (int i = 0; i <= 4; i++){ // Belegung Feld A
+        felda[i] = 1+(i*2);
+    }
+    for (int i = 0; i <= 5; i++){
+        feldb[i] = 2+(i*2);
+    }
+
+    int tickera = 0;
+    int tickerb = 0;
+
+    while(tickera <= 4 and tickerb <= 5){
+        int sum = tickera + tickerb;
+        if(tickerb == 5){
+            feldc[sum] = felda[tickera];
+            tickera++;
+        }
+        if(tickera <= 3 && tickerb <=4){
+            if (felda[tickera] > feldb[tickerb] and tickerb < 5) {
+                feldc[sum] = feldb[tickerb];
+                tickerb++;
+            } else {
+                feldc[sum] = felda[tickera];
+                tickera++;
+            }
+        }else if(tickerb < 5){
+            feldc[sum] = feldb[tickerb];
+            tickerb++;
+        }
+
+
+    }
+
+    for(int i = 0; i<9; i++){
+        int n = feldc[i];
+        printf("%d\n", n);
+    }
+
     return 0;
 }
